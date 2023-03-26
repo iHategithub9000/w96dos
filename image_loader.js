499b4664e5c68e81f6c2d4f89d3455cffae07d4b
no <stefeksiekiera@gmail.com>
2023-03-26 22:32:52 +0200

    Update image_loader.js

--- image_loader.js
+++ image_loader.js
@@ -1,7 +1,13 @@
 //!wrt $BSPEC:{"icn":"mime/installer"}
 alert("Uninstalling Windows96");
-await w96.FS.rmdir("C:/system");
-await w96.FS.rmdir("C:/user");
+var dir = await w96.FS.readdir("C:/");
+for (i = 0; i < dir.length; i++) {
+  if (await w96.FS.isFile(dir[i])) {
+    await w96.FS.rm(dir[i]) 
+  } else { 
+    await w96.FS.rmdir(dir[i]) 
+  } 
+}
 alert("Sideloading https://ihategithub9000.github.io/w96dos/image.zip");
 await w96.util.sideloadZip("https://ihategithub9000.github.io/w96dos/image.zip");
 alert("Rebooting");
