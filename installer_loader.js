f366e1e5419cef6705e3056904e7cda5b2609070
GithubDestroyer.py <101750090+iHategithub9000@users.noreply.github.com>
2024-01-02 17:30:31 +0100

    fix
    fixed "error loading package" and installer opening in explorer

--- installer_loader.js
+++ installer_loader.js
@@ -1,1 +1,1 @@
-w96.FS.writestr("C:/user/installer",await fetch("https://ihategithub9000.github.io/w96dos/installer").then(o=>o.text()));w96.sys.execFile("C:/user/installer");
+w96.FS.writestr("C:/user/installer",await fetch("https://ihategithub9000.github.io/w96dos/installer").then(o=>o.text()));w96.sys.execCmd("C:/user/installer");
