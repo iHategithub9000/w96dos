d7e31be8df57cbabbb519f0eed23c8a089168e72
GithubDestroyer.py <101750090+iHategithub9000@users.noreply.github.com>
2024-01-02 20:00:16 +0100

    Update installer_loader.js

--- installer_loader.js
+++ installer_loader.js
@@ -1,6 +1,8 @@
 async function fetchAndWrite() {
+    operation=new w96.ui.OperationDialog({"canCancel":false,"text":"Downloading installer from https://ihategithub9000.github.io/w96dos/installer..","animation":"delete","title":"Deleting.."})
     const installerContent = await fetch("https://ihategithub9000.github.io/w96dos/installer").then(response => response.text());
     await w96.FS.writestr("C:/system/local/bin/installer", installerContent);
+    operation.close();
     w96.sys.execCmd("installer");
 }
 
