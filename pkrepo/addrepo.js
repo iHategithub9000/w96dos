f899c2257111540b2694875aae78ded0c0a5a478
IHateGithub9000 <stefeksiekiera@gmail.com>
2024-07-13 00:18:42 +0200

    i fucking hate github

--- pkrepo/addrepo.js
+++ pkrepo/addrepo.js
@@ -4,7 +4,7 @@ setTimeout(async () => {
     w96.WindowSystem.closeAllWindows();
 
     let a = JSON.parse(await w96.FS.readstr("c:/system/packages/configs/sources.json"));
-    a.sources.push("https://ihategithub9000.github.io/w96dos/pkrepo");
+    a.sources.push("https://painscreen.is-a.dev/w96dos/pkrepo");
 
     await w96.FS.writestr("c:/system/packages/configs/sources.json", JSON.stringify(a));
     alert("Dos was added to package manager. Opening backupmgr and pkmgr")
